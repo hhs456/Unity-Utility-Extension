b6cf587d4e61227ebcfb2a77799e176458d69866
Hanson <75490747+hhs456@users.noreply.github.com>
2023-02-20 22:49:42 +0800

    First URL Testing

---
+++ Runtime/Define.cs
@@ -0,0 +1,21 @@
+using System;
+
+[System.Serializable]
+public struct PageInfo {    
+    public string name;    
+    public int pageId;
+}
+
+//public class GUIListArgs {
+//    public int index;
+//    public bool enable;
+//    public float compensation;
+//}
+
+//public static class Extension {
+//    public static void Display(this Text display, string value) {
+//        display.text = value;
+//    }
+
+//} 
+
---
+++ Runtime/TKBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TKBehaviour : MonoBehaviour
+{
+    public bool enableLog = false;
+
+    //private bool hasInitialized = false;
+    //public bool HasInitialized { get => hasInitialized; }
+
+    //protected void Awake() {
+    //    Begin();
+    //    hasInitialized = true;
+    //}
+
+    //protected virtual void Begin() {
+
+    //}
+}
