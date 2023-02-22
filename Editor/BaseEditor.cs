ebce72fa5738643537d7a4016a3d0a1d63cef68c
Hanson <75490747+hhs456@users.noreply.github.com>
2023-02-22 21:37:38 +0800

    Add .asmdef

--- Editor/BaseEditor.cs
+++ Editor/BaseEditor.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace ToolKid {
+namespace ToolKid.EditorExtension {
     
     public abstract class BaseEditor : Editor {
 
--- Editor/LabelAttributeDrawer.cs
+++ Editor/LabelAttributeDrawer.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-namespace ToolKid.GUIToolkit {
+namespace ToolKid.EditorExtension {
     [CustomPropertyDrawer(typeof(LabelAttribute))]
     public class LabelAttributeDrawer : PropertyDrawer {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
